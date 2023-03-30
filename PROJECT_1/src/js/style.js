5d9d38c7b0cd135f742d100219a523d8b8f4d533
leo5605 <129360071+leo5605@users.noreply.github.com>
2023-03-30 12:18:12 +0700

    Add files via upload

---
+++ PROJECT_1/src/js/style.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function() {
+    return gulp.src('src/sass/*.+(scss|sass')
+        .pipe(sass(sass({outputStyle: 'compressed'}).on('error', sass.logError)))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+          }))
+        .pipe(autoprefixer({
+			cascade: false
+		}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/sass/*.+(scss|sass', gulp.parallel('styles'))
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
